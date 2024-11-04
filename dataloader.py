6c772658a6d7ba7fb03dddd9b46c6f87b3d00383
Coffee <62540049+realZiweiZhang@users.noreply.github.com>
2024-11-03 21:53:12 -0500

    modified function:lines2word

--- dataloader.py
+++ dataloader.py
@@ -11,10 +11,12 @@ special_tokens = {
 word2idx = {}
 idx2word = {}
 
-idx2char = {1:special_tokens['START'], 2:spectial_tokens['END']}
-char2idx = {special_tokens['START']:1, spectial_tokens['END']:2}
+idx2char = {1:special_tokens['START'], 2:spectial_tokens['END']} # global
+char2idx = {special_tokens['START']:1, spectial_tokens['END']:2} # global
 # char2idx[special_tokens['START']]=1
 # char2idx[special_tokens['END']]=2
+
+output_chars = {}
 
 def get_lines_from_txt(split=None):
     assert split is not None, "split is not speficied"
@@ -28,11 +30,15 @@ def get_lines_from_txt(split=None):
     print(lines[2])
     return lines
     
-    
 def lines2word(lines):
     num_word = 0
-    output_idx = []
-    for line in lines:
+    output_dict = {}
+    output_word_idx = [] # local
+    output_chars = []
+    
+    for i, line in enumerate(lines):
+        line_word_idx = []
+        line_chars = []
         # Only words containing characters and numbers are retained, and special symbols are eliminated, e.g. -
         words = re.findall(r'\b\w+\b', line)
         print('sentence being processing',line)
@@ -47,8 +53,16 @@ def lines2word(lines):
                 idx2word[len(idx2word)+1] = word 
                 word2idx[word] = len(idx)
                 
-            output_idx.append(word2idx[word])
+            output_word_idx.append(word2idx[word])
+            line_word_idx.append(word2idx[word])
             
+            chars = word2char(word)
+            line_chars.append(chars)
+            output_chars.append(chars)
+        output_dict[i] = {'word_idx': line_word_idx, 'chars': line_chars}
+    
+    output_dict['all_word_idx':output_word_idx, 'all_chars': output_chars]
+    return output_dict
 
 def word2char(word): 
     chars = {0:special_tokens['START']}
@@ -59,8 +73,18 @@ def word2char(word):
 
         chars[len(chars)+1] = char2idx[char]
     chars[len(chars)+1] = char2idx[special_tokens['END']]
+
+    return chars
     
-    #TODO length-0p;.67
+#TODO length
+def max_length_truncation(chars, max_length):
+    pass
+    
+def char_dataset_generation(split):
+    split_lines = get_lines_from_txt(split)
+    split_words_id = lines2word(split_lines)
+    
+    for 
 if __name__ == '__main__':
     train_lines = get_lines_from_txt('train')
     
